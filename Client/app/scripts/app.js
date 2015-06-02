077436b435c9fb8e119b62fc6374f9775dbd81a0
anupsaw <anupsaw@gmail.com>
2015-06-02 00:52:10 -0400

    New changes
    Added some more files

---
+++ Client/app/scripts/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+var ClientApp = angular.module('clientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  
+  ClientApp.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      }) 
+	  .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      }) 
+	  .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+  
+  
+  var controllers = {};   //JavaScript Object
+//adding the controllers to InsuredPortalApp angularjs app
+ClientApp.controller(controllers);
---
+++ Client/app/scripts/controllers/register.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clientApp
+ */
+  
+ controllers.RegisterCtrl =  function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }
